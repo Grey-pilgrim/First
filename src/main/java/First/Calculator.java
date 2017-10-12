944cea69dc341d0338f9380563316dce306c0ae1
Grey-pilgrim <Grey_pilgrim@mail.ru>
2017-10-13 00:10:56 +0700

    Next update

--- src/main/java/First/Calculator.java
+++ src/main/java/First/Calculator.java
@@ -14,13 +14,13 @@ public class Calculator
     {
         double a, b, c;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите два дробных числа");
-        System.out.println("Введите первое число");
+        System.out.println("Введите два дробных числа"); //Системное сообщение
+        System.out.println("Введите первое число");//Системное сообщение
         a = sc.nextDouble();
-        System.out.println("Введите второе число");
+        System.out.println("Введите второе число");//Системное сообщение
         b = sc.nextDouble();
         c = a + b;
-        System.out.printf ("Сумма ваших чисел равна " + "%-8.4f", c);
+        System.out.printf ("Сумма ваших чисел равна " + "%-8.4f", c); //Системное сообщение
 
         }
 }
