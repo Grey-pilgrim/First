17a1c52375712ce02da70558f1ad526982e6e8b5
Grey-pilgrim <Grey_pilgrim@mail.ru>
2017-10-15 17:52:30 +0700

    Second calk

---
+++ src/main/java/First/Start.java
@@ -0,0 +1,27 @@
+package First;
+
+import java.util.Scanner;
+
+public class Start {
+
+    public static void main (String[]args) {
+        System.out.println("Выберите операцию:");
+        System.out.print("1 - калькулятор\t");
+        System.out.println("2 - сравнение множества слов\t");
+        Scanner sc = new Scanner(System.in);
+        int w;
+        w = sc.nextInt();
+        switch(w) {
+            case 1: {
+                CalculatorAdamov.run();
+                break;
+            }
+            case 2: {
+                Many.run();
+                break;
+            }
+            default: System.out.println("Недопустимая операция!!!");
+                break;
+        }
+    }
+}
