1a8ce2ad144a207aaf835d88d13f6de1e522c21f
Grey-pilgrim <Grey_pilgrim@mail.ru>
2017-10-16 21:42:34 +0700

    Second

--- src/main/java/First/Many.java
+++ src/main/java/First/Many.java
@@ -10,27 +10,26 @@ package First;
 import java.util.Scanner;
 public class Many {
     public static void run() {
+        System.out.println("Введите несколько слов различной длины");
         Scanner in = new Scanner(System.in);
-        String str = "",max = "";
+        final char underLine = '_';
+        final char nextLine = '\n';
+        String line, max = "";
+        int start = 0;
         while (in.hasNextLine()){
-            str += " " + in.nextLine();
-        }
-        String[] strL = new String[10]; //массив слов
-        for (int i = 0; i<strL.length; i++) {
-            strL[i] = "";
-        }
-        for (int i=0,k=0;i<str.length(); i++){
-            int chr = str.charAt(i); //номер символа
-            if ((chr > 47 && chr < 58) || (chr > 64 && chr < 91 ) || (chr > 96 && chr < 123) || (chr == 95)){
-                strL[k] = strL[k]+str.charAt(i); // проверяю, находится ли символ, разделяющий слова, если нет, прибавляю символ к слову, иначе, перехожу к следующему
+            line = in.nextLine();
+            for (int i=0; i<line.length(); i++){
+                int chr = line.charAt(i); //номер символа
+                if ((chr > 47 && chr < 58) || (chr > 64 && chr < 91 ) || (chr > 96 && chr < 123) || (chr == 95)){
+                } else {
+                    if ( i - start > max.length() ) {
+                        max = line.substring( start, i );
+                    }
+                    start = i+1;
+                }
             }
-            else {k++;}
+            start = 0;
         }
-        for (String aStrL : strL) {        //нахожу наибольшее слово
-            if (aStrL.length() > max.length()) {
-                max = aStrL;
-            }
-        }
-        System.out.println(max);
+        System.out.println("Самое длинное слово" + max);
     }
 }
