b03242660bae50f118906352023c3aff43a65f68
Grey-pilgrim <Grey_pilgrim@mail.ru>
2017-11-08 00:24:45 +0600

    Test OpMinus

---
+++ src/test/java/First/calculator/OpMinusTest.java
@@ -0,0 +1,20 @@
+package First.calculator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OpMinusTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void exec() throws Exception {
+    }
+
+}
